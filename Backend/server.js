9a6cbb6c3584d24caa55c459f4a9d9e0a5c32f63
Arpana bharti <arpanabharti09@gmail.com>
2026-03-16 12:24:54 +0530

    socket.io integration done

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,14 @@ import 'dotenv/config'
 import app from './src/app.js'
 import connectToDb from './src/config/database.js';
 import { testAi } from './src/services/ai.sevices.js';
+import http from 'http'
+import { initSocket } from './src/sockets/server.socket.js';
 
 connectToDb()
 testAi()
+const httpServer = http.createServer(app)
+initSocket(httpServer)
 
-app.listen(3000,()=>{
+httpServer.listen(3000,()=>{
     console.log("Server running on port 3000"); 
 })
---
+++ Backend/src/sockets/server.socket.js
@@ -0,0 +1,27 @@
+import {Server, Socket} from 'socket.io'
+
+let io;
+export function initSocket(httpServer){
+    io=new Server(httpServer,{
+        cors:{
+            origin:'http://localhost:5173',
+            credentials:true
+        }
+    })
+
+    console.log("socket.io server is running");
+
+    io.on("connection",(socket)=>{
+        console.log("A user connected: "+ socket.id);
+        
+    })
+    
+
+}
+
+export function getIo(){
+    if(!io){
+        throw new Error("socket.io not initailized")
+    }
+    return io
+}
